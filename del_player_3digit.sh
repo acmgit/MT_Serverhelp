1bcbc58906ea000664505c1537ba625493747ac0
A.C.M <undertakers_help@yahoo.com>
2018-12-22 16:00:01 +0100

    Bugfix from Gundul (berengma).

--- del_player_3digit.sh
+++ del_player_3digit.sh
@@ -8,20 +8,20 @@
 #
 
 # Generate a List of Players with 3 Digits in their name
-sed 's/[0-9][0-9][0-9]*/&\n/' <$1 | grep -v ":" >players/del_player.txt
+sed 's/[0-9][0-9][0-9]*/&\n/' <$1 | grep -v ":" | grep -P '(?<!\d)\d{3}(?!\d)' >players/del_player.txt
+
 
 # Generate the clean File
 grep -vf players/del_player.txt $1 >$1_new
 
 # Remove the Player-Files
 cd players
-rm $(cat del_player.txt)
+rm $(cat del_player.txt) >> NUL
 cd ..
 
-# Remove the old File
-rm $1
-# and restore it with the clean File.
+# Backup old file
+mv $1 "authBackup"
+
+# replace old file with new cleaned one
 mv $1_new $1
-
-
 
